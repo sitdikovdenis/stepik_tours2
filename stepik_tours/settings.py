cfc6e2581b799603d7e6d9b238823ed564ba9cf2
sitdikovdenis <fomk@mail.ru>
2020-05-07 11:42:14 +0300

    bag fixed

--- stepik_tours/settings.py
+++ stepik_tours/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_8y(d@t4&g_j1fs@68%xxfpap&5cug!k0gnntp4k*h&62v0t5y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
